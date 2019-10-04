da9794414a194807692ddabc3e32d713014ffb90
zachary-peasley <zpman95@hotmail.com>
2019-10-04 10:37:17 -0500

    Put in middleware

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -25,3 +25,46 @@ router.put('/:id', validateProjectID, ValiditeProject, (req,res) => {
         res.status(500).json({error: 'errors'})
     })
 })
+
+router.post('/', validateProject, (req,res) => {
+    const project = req.body;
+    projectDatebase.insert(project)
+    .then(results => {
+        res.status(200).json(results)
+    })
+    .catch(error => {
+        res.status(500).json({error: "error"})
+    })
+})
+
+function validateProjectID (req,res,next) {
+    const id =req.params.id
+    projectDatebase.get(id)
+    .then(results => {
+        if(results) {
+            next()
+    } else {
+        res.status(404).json({message: "The specified Project doesn't exist"})
+    }
+})
+.catch(error => {
+    res.status(500).json({error: "error"})
+})
+}
+
+function validateProject(req, res, next) {
+    const project = req.body;
+    console.log(project)
+    if(!project.name) {
+        res.status(400).json({message: "Name require"})
+    } else if(!project.description) {
+        res.status(400).json({message: " Description require"})
+    } else if(!project) {
+        res.status(400).json({message: "Invaild project details"})
+
+    } else {
+        next()
+    }
+}
+
+module.exports = router;
