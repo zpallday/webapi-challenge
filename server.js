287a0d5f3a358d9b23eb5a39fe3d2d552245eee7
zachary-peasley <zpman95@hotmail.com>
2019-10-04 11:09:35 -0500

    Action Router works

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const server = express();
 
 const porjectsRoutes = require('./projects/projectRouter');
+const actionRoutes = require('./actions/actionRouter')
 
 server.use('/projects', porjectsRoutes)
+server.use('/actions', actionRoutes)
 
 module.exports = server;
